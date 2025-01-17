0de0946da8ec44b8a810678935e2da09fb0a64d2
Zahin_Abdullah <zahinabdullahrakin@gmail.com>
2025-01-17 10:09:14 +0600

    added logging mechanism, multer for multipart form handling, cloudinary for hosting files.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+
+import logger from './utils/logger.js';
+import { errorHandler } from './middlewares/error.middlewares.js';
+import healthCheckRouter from './routes/healthCheck.routes.js';
+
 
 const app = express();
 app.use(cors(
@@ -8,15 +15,28 @@ app.use(cors(
     credentials: true
   }
 ));
-//common middleware
 app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({extended: true, limit: '16kb'}));
 app.use(express.static('public'));
-
-//import routes
-import healthCheckRouter from './routes/healthCheck.routes.js';
+app.use(cookieParser());
+const morganFormat = ':method :url :status :response-time ms';
+app.use(morgan(morganFormat, {
+  stream: {
+    write: (message) => {
+      const logObject = {
+        method: message.split(' ')[0],
+        url: message.split(' ')[1],
+        status: message.split(' ')[2],
+        responseTime: message.split(' ')[3]
+      };
+      logger.info(JSON.stringify(logObject));
+    }
+  }
+}));
 
 // routes
 app.use("/api/v1/healthcheck", healthCheckRouter);
 
+
+app.use(errorHandler);
 export { app };
