3e0d7c1a55d874df1572851c159c29577497e16f
Zahin_Abdullah <zahinabdullahrakin@gmail.com>
2025-01-26 08:49:17 +0600

    added the model of notifications

---
+++ src/models/notification.models.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const notificationSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true,
+  },
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: false, 
+  },
+  listingId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Listing', 
+    required: true,
+  },
+  message: {
+    type: String, 
+    required: true,
+  },
+  isRead: {
+    type: Boolean, 
+    default: false,
+  },
+  type: {
+    type: String,
+    enum: ['BOOKING_REQUEST', 'BOOKING_APPROVED', 'BOOKING_DECLINED', 'WISHLIST', 'GENERAL'],
+    required: true
+  },
+  timestamp: {
+    type: Date, 
+    default: Date.now,
+  },
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
